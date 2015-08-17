0bfd8f6c806327f9e1fe2856e1ad81381a6c810a
sjpence <sjpence00@gmail.com>
2015-08-17 00:44:59 -0400

    Added Glimpse and updated Bootstrap to 3.3.5. Reverted theme to default.

--- unitdesign-web/App_Start/BundleConfig.cs
+++ unitdesign-web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace unitdesign_web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-theme.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
