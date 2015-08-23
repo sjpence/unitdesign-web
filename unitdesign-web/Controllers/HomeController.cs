5f4bff36493bb74b0576e51b575a06fbde86b60a
sjpence <sjpence00@gmail.com>
2015-08-23 15:57:28 -0400

    Added About content from old site. Added address, phone, email to contact page.

--- unitdesign-web/Controllers/HomeController.cs
+++ unitdesign-web/Controllers/HomeController.cs
@@ -14,16 +14,12 @@ namespace unitdesign_web.Controllers
         }
 
         public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
+        {            
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
