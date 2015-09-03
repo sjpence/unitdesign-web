802d04a9bc89fa0d7574a7d153ad4a76b75cb46a
sjpence <sjpence00@gmail.com>
2015-09-03 12:03:52 -0400

    Fix form layout so controls stretch to full width. Make comments multiline input. Add email validation.

--- unitdesign-web/Models/ContactViewModel.cs
+++ unitdesign-web/Models/ContactViewModel.cs
@@ -14,6 +14,7 @@ namespace unitdesign_web.Models
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Valid email address is required")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         public string Phone { get; set; }
@@ -26,6 +27,7 @@ namespace unitdesign_web.Models
         public string Country { get; set; }
 
         [Required(ErrorMessage = "Comments are required")]
+        [DataType(DataType.MultilineText)]
         public string Comments { get; set; }
 
         public DateTime TimeSent { get; set; }
