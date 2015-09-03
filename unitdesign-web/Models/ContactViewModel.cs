785d742835e1e52bb3d4aab71cc8620a17111aa0
sjpence <sjpence00@gmail.com>
2015-09-03 10:00:52 -0400

    Created a basic contact view model class.

---
+++ unitdesign-web/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace unitdesign_web.Models
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Company { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Comments { get; set; }
+        public DateTime TimeSent { get; set; }
+    }
+}
